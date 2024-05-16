ab373eacee2e8db74388fa8bbc82da0f5f0718da
Gideon McKinlay <gmckinlay04@gmail.com>
2024-05-16 14:06:07 +1000

    Some changes

--- METR2800-Zebras.ino
+++ METR2800-Zebras.ino
@@ -14,14 +14,17 @@
  ====================
    METR2800 ZEBRAS
  Alistair, Oliver, Bailey, Wilbur, Gideon, Jestin
- Modified 2024-04-26
+ Modified 2024-05-15
  By Gideon McKinlay
  */
 
+/*
+ * 
+ */
 enum Speed {
-  walk = 100,
-  trot = 10,
-  canter = 5,
+  walk = 10,
+  trot = 5,
+  canter = 2,
   gallop = 1
 };
 
@@ -29,15 +32,19 @@ Speed speed = trot;
 
 // FT5116M Servos
 #define ARM_EXTENSION_PIN 23
-#define ELEVATOR_PIN 11
+#define ELEVATOR_PIN1 11
+#define ELEVATOR_PIN2 32
 // A4988 Stepper
 #define ROTATING_PLATE_DIR 12
-#define ROTATING_PLATE_STEP 14
+#define ROTATING_PLATE_STEP 13
 #define STEPS 200
+#define GEAR_RATIO1 200
+#define GEAR_RATIO2 24
+#define MICRO_STEPS 4
 // FIT0521 DC
 #define DC_MOTOR_PIN1 26
 #define DC_MOTOR_PIN2 27
-#define DC_MOTOR_ENABLE 13
+#define DC_MOTOR_ENABLE 14
 // Servo
 #define LATCH_SERVO_PIN 4
 #define TILT_SERVO_PIN 5
@@ -75,61 +82,106 @@ int sign(int x) {
   return 0;
 }
 
+int16_t degToCycles(float degrees) {
+  /*
+   * Converts a (float) number of degrees into
+   * the number of cycles needed to rotate that angle.
+   */
+  float n = (degrees * MICRO_STEPS * STEPS * GEAR_RATIO1)/(360 * GEAR_RATIO2);
+  return (n + 0.5f);
+}
+
 void setup() {
   // Attach servos to pins
   arm_extension.attach(ARM_EXTENSION_PIN);
   elevator.attach(ELEVATOR_PIN);
-
+  latch.attach(LATCH_SERVO_PIN);
+  // Set stepper outputs
   pinMode(ROTATING_PLATE_DIR, OUTPUT);
   pinMode(ROTATING_PLATE_STEP, OUTPUT);
-  
+  // Set DC motor outputs
   pinMode(DC_MOTOR_PIN1, OUTPUT);
   pinMode(DC_MOTOR_PIN2, OUTPUT);
   pinMode(DC_MOTOR_ENABLE, OUTPUT);
-
+  // Set limit switch to input
+  pinMode(LIMIT_SWITCH_PIN, INPUT_PULLDOWN);
+  // Reset extension and height variables
   currentExtension = 0;
   currentHeight = 0;
-  currentRotation = 0;
-
-  pinMode(LIMIT_SWITCH_PIN, INPUT_PULLDOWN);
 }
 
 void armExtendTo(uint16_t distance) {
-  for (int i = currentExtension; i != distance; i += sign(distance-currentExtension)) {
-    arm_extension.write(i);
-    currentExtension = i;
-    delay(speed);
-  }
+  /*
+   * armExtendTo()
+   * =============
+   * Extends the arm radially to the given distance.
+   * distance is an integer number in arbitrary units.
+   */
+  uint16_t movementDuration = (distance-currentExtension)/speed;
+  uint16_t movementMagnitude = speed*sign(distance-currentExtension);
+  arm_extension.write(90 + movementMagnitude);
+  delay(movementDuration);
+  arm_extension.write(90);
   currentExtension = distance;
 }
 
 void changeHeightTo(uint16_t height) {
-  for (int i = currentHeight; i != height; i += sign(height-currentHeight)) {
-    elevator.write(i);
-    currentHeight = i;
-    delay(speed);
-  }
+  /*
+   * changeHeightTo()
+   * ================
+   * Raises the arm vertically to the given height.
+   * height is an integer number in arbitrary units.
+   */
+  uint16_t movementDuration = speed*(height-currentHeight);
+  uint16_t movementMagnitude = speed*sign(height-currentHeight);
+  elevator.write(90 + movementMagnitude);
+  delay(movementDuration);
+  elevator.write(90);
   currentHeight = height;
 }
 
-void armRotateTo(uint8_t point, uint8_t direction) {
+void armRotateBy(int16_t cycles) {
+  /*
+   * armRotateBy()
+   * =============
+   * Rotates the rotating plate by the given number of cycles.
+   * Positive values result in clockwise motion,
+   * and negative values result in anticlockwise motion.
+   *
+   * cycles = angle * GEAR_RATIO * MICRO_STEPS * STEPS * 1/360
+   * 360 degrees is 6667 points.
+   */
+  // int16_t cycles = degToCycles(degrees);
   // Choose the direction
+  uint8_t direction = (cycles > 0) ? CLOCKWISE : ANTICLOCKWISE;
   digitalWrite(ROTATING_PLATE_DIR, direction);
-  // TODO: Use >= or <= for comparison
-  while (currentRotation != point) {
-    digitalWrite(ROTATING_PLATE_STEP, HIGH); // Move one step
+  // Repeat for the given number of cycles.
+  for(uint16_t x = 0; x <= abs(cycles); x++) {
+    digitalWrite(ROTATING_PLATE_STEP, HIGH);
     delay(speed);
-    digitalWrite(ROTATING_PLATE_STEP, LOW); // Move one step
-    currentRotation = (currentRotation+direction) % STEPS;
+    digitalWrite(ROTATING_PLATE_STEP, LOW);
+    // currentRotation = (currentRotation+direction) % STEPS;
     delay(speed);
   }
 }
 
-void openLatch() {
-  latch.write(0);
+void openLatch(uint_8 angle = 0) {
+  /*
+   * openLatch()
+   * ===========
+   * Opens the collection latch.
+   * If an angle is specified it is opened to 
+   */
+  latch.write(angle);
   latchOpen = true;
 }
+
 void closeLatch() {
+  /*
+   * closeLatch()
+   * ===========
+   * Closes the collection latch.
+   */
   latch.write(180);
   latchOpen = false;
 }
@@ -164,8 +216,15 @@ void waitForIRSense() {
   Serial.println(" IR: DETECTED");
 }
 
-void moveToPosition(uint8_t rotation, uint8_t direction, uint8_t extension, uint8_t elevation) {
-  std::thread t1(armRotateTo, rotation, direction);
+void moveToPosition(uint8_t rotation, uint8_t extension, uint8_t elevation) {
+  /*
+    moveToPosition()
+    ================
+    Moves the rotating plate to the given rotation, extends the arm to the given extension,
+    and raises the arm to the given elevation.
+   */
+  // Uses three threads to do the three motions so that the resultant motion occurs in parallel.
+  std::thread t1(armRotateBy, rotation);
   std::thread t2(armExtendTo, extension);
   std::thread t3(changeHeightTo, elevation);
   t1.join();
@@ -181,27 +240,27 @@ void loop() {
   }
 
   // Move to first target
-  moveToPosition(0, CLOCKWISE, 180, 180);
+  moveToPosition(90, 100, 0);
   closeLatch();
   // Move to hole
-  moveToPosition(90, ANTICLOCKWISE, 90, 0);
+  moveToPosition(90, 90, 0);
   openLatch();
   // Move to second target
-  moveToPosition(0, CLOCKWISE, 180, 180);
+  moveToPosition(135, CLOCKWISE, 180, 180);
   closeLatch();
   // Move to hole
-  moveToPosition(90, ANTICLOCKWISE, 90, 0);
+  moveToPosition(-135, ANTICLOCKWISE, 90, 0);
   openLatch();
   // Move to third target
-  moveToPosition(0, CLOCKWISE, 180, 180);
+  moveToPosition(180, CLOCKWISE, 180, 180);
   closeLatch();
   // Move to hole
-  moveToPosition(90, ANTICLOCKWISE, 90, 0);
+  moveToPosition(-180, ANTICLOCKWISE, 90, 0);
   openLatch();
 
   // Drive to opposite side
   std::thread t1(driveToSide);
-  std::thread t2(armRotateTo, 0, CLOCKWISE);
+  std::thread t2(armRotateBy, 0, CLOCKWISE);
   std::thread t3(armExtendTo, 180);
   std::thread t4(changeHeightTo, 180);
   t1.join();
