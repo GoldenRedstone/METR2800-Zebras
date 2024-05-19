a0ea3c2dc9cb400d6d2068ad0d9a469f3cc30e32
GoldenRedstone <71370193+GoldenRedstone@users.noreply.github.com>
2024-05-19 13:44:19 +1000

    Update demo_functionality.ino

--- demo_functionality.ino
+++ demo_functionality.ino
@@ -1,5 +1,9 @@
 #include <ESP32Servo.h>
 
+// FT5116M Servos
+#define ARM_EXTENSION_PIN 23
+#define ELEVATOR1_PIN 33
+#define ELEVATOR2_PIN 32
 // A4988 Stepper
 #define ROTATING_PLATE_DIR 12
 #define ROTATING_PLATE_STEP 13
@@ -17,217 +21,250 @@
 #define CLOCKWISE HIGH
 #define ANTICLOCKWISE LOW
 
-uint8_t speed = 50;
+uint8_t ex_speed = 50;
+uint8_t el_speed = 20;
 uint8_t freq = 2;
 
 Servo arm_extension;
+Servo elevator1;
+Servo elevator2;
 Servo latch;
 
 bool latchOpen = false;
 
 void setup() {
-  // put your setup code here, to run once:
-  // Set Stepper outputs
-  pinMode(ROTATING_PLATE_DIR, OUTPUT);
-  pinMode(ROTATING_PLATE_STEP, OUTPUT);
+    // put your setup code here, to run once:
+    // Set Stepper outputs
+    pinMode(ROTATING_PLATE_DIR, OUTPUT);
+    pinMode(ROTATING_PLATE_STEP, OUTPUT);
 
-  // Set DC motor outputs
-  pinMode(DC_MOTOR_PIN1, OUTPUT);
-  pinMode(DC_MOTOR_PIN2, OUTPUT);
-  pinMode(DC_MOTOR_ENABLE, OUTPUT);
+    // Set DC motor outputs
+    pinMode(DC_MOTOR_PIN1, OUTPUT);
+    pinMode(DC_MOTOR_PIN2, OUTPUT);
+    pinMode(DC_MOTOR_ENABLE, OUTPUT);
 
-  latch.attach(LATCH_SERVO_PIN);
-  arm_extension.attach(ARM_EXTENSION_PIN);
+    latch.attach(LATCH_SERVO_PIN);
+    arm_extension.attach(ARM_EXTENSION_PIN);
+    elevator1.attach(ELEVATOR1_PIN);
+    elevator2.attach(ELEVATOR2_PIN);
 
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  // Demonstrate latch and stepper works
-  delay(500);
-  latch.write(120);
-  latchOpen = false;
-  delay(500);
-  latch.write(0);
-  latchOpen = true;
+    // Demonstrate latch and stepper works
+    delay(500);
+    latch.write(120);
+    latchOpen = false;
+    delay(500);
+    latch.write(0);
+    latchOpen = true;
 
-  delay(500);
-  armRotateBy(45);
-  delay(500);
-  armRotateBy(-45);
+    delay(500);
+    armRotateBy(45);
+    delay(500);
+    armRotateBy(-45);
 
-  Serial.println("Started listening");
+    Serial.println("Started listening");
 }
 
 int16_t degToCycles(float degrees) {
-  /*
-   * degToCycles()
-   * =============
-   * Converts a (float) number of degrees into
-   * the number of cycles needed to rotate that angle.
-   * 
-   * cycles = angle/360 * GEAR_RATIO * MICRO_STEPS * STEPS
-   * At our chosen values 360 degrees is 6667 points.
-   */
-  float n = (degrees * MICRO_STEPS * STEPS * GEAR_RATIO1)/(360 * GEAR_RATIO2);
-  return (n + 0.5f);
+    /*
+     * degToCycles()
+     * =============
+     * Converts a (float) number of degrees into
+     * the number of cycles needed to rotate that angle.
+     * 
+     * cycles = angle/360 * GEAR_RATIO * MICRO_STEPS * STEPS
+     * At our chosen values 360 degrees is 6667 points.
+     */
+    float n = (degrees * MICRO_STEPS * STEPS * GEAR_RATIO1) / (360 * GEAR_RATIO2);
+    return (n + 0.5f);
 }
 
 void armRotateBy(float degrees) {
-  /*
-   * armRotateBy()
-   * =============
-   * Rotates the rotating plate by the given number of degrees.
-   * Positive values result in clockwise motion,
-   * negative values result in anticlockwise motion.
-   */
-  int16_t cycles = degToCycles(degrees);
-  // Choose the direction
-  uint8_t direction = (cycles > 0) ? CLOCKWISE : ANTICLOCKWISE;
-  digitalWrite(ROTATING_PLATE_DIR, direction);
-  for (uint16_t x = 0; x <= abs(cycles); x++) {
-    digitalWrite(ROTATING_PLATE_STEP, HIGH);
-    delay(freq);
-    digitalWrite(ROTATING_PLATE_STEP, LOW);
-    // currentRotation = (currentRotation+direction) % STEPS;
-    delay(freq);
-  }
+    /*
+     * armRotateBy()
+     * =============
+     * Rotates the rotating plate by the given number of degrees.
+     * Positive values result in clockwise motion,
+     * negative values result in anticlockwise motion.
+     */
+    int16_t cycles = degToCycles(degrees);
+    // Choose the direction
+    uint8_t direction = (cycles > 0) ? CLOCKWISE : ANTICLOCKWISE;
+    digitalWrite(ROTATING_PLATE_DIR, direction);
+    for (uint16_t x = 0; x <= abs(cycles); x++) {
+        digitalWrite(ROTATING_PLATE_STEP, HIGH);
+        delay(freq);
+        digitalWrite(ROTATING_PLATE_STEP, LOW);
+        // currentRotation = (currentRotation+direction) % STEPS;
+        delay(freq);
+    }
 }
 
 void openLatch(uint8_t angle = 0) {
-  /*
-   * openLatch()
-   * ===========
-   * Opens the collection latch.
-   * If an angle is specified it is opened to that angle.
-   */
-  latch.write(angle);
-  latchOpen = true;
+    /*
+     * openLatch()
+     * ===========
+     * Opens the collection latch.
+     * If an angle is specified it is opened to that angle.
+     */
+    latch.write(angle);
+    latchOpen = true;
 }
 
 void closeLatch(uint8_t angle = 120) {
-  /*
-   * closeLatch()
-   * ===========
-   * Closes the collection latch.
-   * If an angle is specified it is opened to that angle.
-   */
-  latch.write(angle);
-  latchOpen = false;
+    /*
+     * closeLatch()
+     * ===========
+     * Closes the collection latch.
+     * If an angle is specified it is opened to that angle.
+     */
+    latch.write(angle);
+    latchOpen = false;
 }
 
 uint8_t mode = 0;
 uint16_t value = 0;
+int8_t dir = 1;
+
+uint8_t reading = false;
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 
-  // Wait for some input.
-  while (!Serial.available()) {
-    delay(10);
-  }
-  int8_t dir = 1;
-  Serial.println("Heard!");
-  while (Serial.available()) {
-    char ch = Serial.read();
-    // If it is a mode selection character then set mode.
-    if (ch == 'L') {
-      Serial.println("Latch");
-      mode = 5;
-    } else if (ch == 'T') {
-      Serial.println("Rotation");
-      mode = 1;
-    } else if (ch == 'X') {
-      Serial.println("Extension");
-      mode = 2;
-    } else if (ch == 'V') {
-      Serial.println("Elevation");
-      mode = 3;
-    } else if (ch == 'D') {
-      Serial.println("Drive");
-      mode = 4;
-    } else if (ch == 'S') {
-      Serial.println("Speed");
-      mode = 11;
-    } else if (ch == 'F') {
-      Serial.println("Frequency");
-      mode = 12;
-    // If it includes '-' then set dir to negative.
-    } else if (ch == '-') {
-      Serial.println("Backwards!");
-      dir = -1;
-    // If it is a number then accumulate the value.
-    } else if (ch >= '0' && ch <= '9') {
-      value = (value * 10) + (ch - '0');  // Accumulate the value
-    // If it is the string terminator then perform the action based on the mode.
-    } else if (ch == 10) {
-      Serial.println(value * dir);
-      if (mode == 1) {
-        Serial.println("Rotation");
-        armRotateBy(value * dir);
+    reading = true;
+    dir = 1;
+    mode = 0;
+    while (reading) {
+        // Wait for some input.
+        while (!Serial.available()) { delay(10); }
+        Serial.flush();
+        
+        Serial.println("Heard!");
+        while (Serial.available()) {
+            char ch = Serial.read();
+            // If it is a mode selection character then set mode.
+            if (ch == 'L') {
+                Serial.println("Latch");
+                mode = 5;
+            } else if (ch == 'T') {
+                Serial.println("Rotation");
+                mode = 1;
+            } else if (ch == 'X') {
+                Serial.println("Extension");
+                mode = 2;
+            } else if (ch == 'V') {
+                Serial.println("Elevation");
+                mode = 3;
+            } else if (ch == 'D') {
+                Serial.println("Drive");
+                mode = 4;
+            } else if (ch == 'S') {
+                Serial.println("Extension Speed");
+                mode = 11;
+            } else if (ch == 'F') {
+                Serial.println("Frequency");
+                mode = 12;
+            } else if (ch == 'Q') {
+                Serial.println("Elevator Speed");
+                mode = 13;
 
-      } else if (mode == 2) {
-        Serial.println("Extension");
-        arm_extension.write(90 + speed*dir);
-        delay(value);
-        arm_extension.write(90);
+            // If it includes '-' then set dir to negative.
+            } else if (ch == '-') {
+                Serial.println("Backwards!");
+                dir = -1;
+            
+            // If it is a number then accumulate the value.
+            } else if (ch >= '0' && ch <= '9') {
+                value = (value * 10) + (ch - '0');  // Accumulate the value
+            
+            // If it is the string terminator then perform the action based on the mode.
+            } else if (ch == 'C') {
+                Serial.println(value * dir);
+                if (mode == 1) {
+                    Serial.println("Rotation");
+                    armRotateBy(value * dir);
 
-      } else if (mode == 3) {
-        Serial.println("Elevation");
-        // No elevation functionality.
+                } else if (mode == 2) {
+                    Serial.println("Extension");
+                    arm_extension.write(90 + ex_speed * dir);
+                    delay(value);
+                    arm_extension.write(90);
 
-      } else if (mode == 4) {
-        Serial.println("Drive");
-        if (dir == 1) {
-          digitalWrite(DC_MOTOR_PIN1, HIGH);
-          digitalWrite(DC_MOTOR_PIN2, LOW);
-        } else {
-          digitalWrite(DC_MOTOR_PIN1, LOW);
-          digitalWrite(DC_MOTOR_PIN2, HIGH);
+                } else if (mode == 3) {
+                    Serial.println("Elevation");
+                    elevator1.write(90 + el_speed * dir);
+                    elevator2.write(90 + el_speed * dir);
+                    delay(value);
+                    elevator1.write(90);
+                    elevator2.write(90);
+
+                } else if (mode == 4) {
+                    Serial.println("Drive");
+                    if (dir >= 0) {
+                        digitalWrite(DC_MOTOR_PIN1, HIGH);
+                        digitalWrite(DC_MOTOR_PIN2, LOW);
+                    } else {
+                        digitalWrite(DC_MOTOR_PIN1, LOW);
+                        digitalWrite(DC_MOTOR_PIN2, HIGH);
+                    }
+                    digitalWrite(DC_MOTOR_ENABLE, HIGH);
+                    delay(value);
+                    digitalWrite(DC_MOTOR_ENABLE, LOW);
+
+                } else if (mode == 5) {
+                    Serial.println("Latch");
+                    if (latchOpen) {
+                        Serial.println("Close");
+                        closeLatch();
+                    } else {
+                        Serial.println("Open");
+                        openLatch();
+                    }
+
+                } else if (mode == 11) {
+                    Serial.println("Extension Speed");
+                    ex_speed = value;
+                    Serial.println(ex_speed);
+                } else if (mode == 12) {
+                    Serial.println("Frequency");
+                    freq = value;
+                    Serial.println(freq);
+                } else if (mode == 13) {
+                    Serial.println("Elevator Speed");
+                    el_speed = value;
+                    Serial.println(el_speed);
+                }
+
+                value = 0;  // reset val to 0 ready for the next sequence of digits
+                reading = false;
+                Serial.println("Finished");
+                Serial.println("");
+            } else {
+                Serial.println((int)ch);
+            }
         }
-        digitalWrite(DC_MOTOR_ENABLE, HIGH);
-        delay(value);
-        digitalWrite(DC_MOTOR_ENABLE, LOW);
-
-      } else if (mode == 5) {
-        Serial.println("Latch");
-        if (latchOpen) {
-          Serial.println("Close");
-          closeLatch();
-        } else {
-          Serial.println("Open");
-          openLatch();
-        }
-
-      } else if (mode == 11) {
-        Serial.println("Speed");
-        speed = value;
-        Serial.println(speed);
-      } else if (mode == 12) {
-        Serial.println("Frequency");
-        freq = value;
-        Serial.println(freq);
-      }
-
-      value = 0;  // reset val to 0 ready for the next sequence of digits
-      Serial.println("Finished");
     }
-  }
 }
 
 /*
  * L - Opens latch
  * T angle - Stepper
- * X ms - Extension
- * V ms - Elevator
+ * X ms - Extension (-outwards, +inwards)
+ * V ms - Elevator (-downwards, +upwards)
  * D ms - Drive
  * F int - Rotating Frequency
- * S int - Servo Speed
+ * S int - Extension Speed
+ * Q int - Elevator Speed
+ * C - Instruction Complete
  *
  * Important examples:
- * L
- * T90
- * T-90
- * X1000
- * X-1000
- * F2
- * S50
+ * LC
+ * T90C
+ * T -90C
+ * X1000C
+ * X -1000C
+ * F2C
+ * S50C
+ * Q20C
  */
